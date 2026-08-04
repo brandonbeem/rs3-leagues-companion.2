358b4efc727980980c451d7504568b3c4685ae9f
brandonbeem <130791945+brandonbeem@users.noreply.github.com>
2026-08-04 08:38:34 -0500

    Merge early task locations into Misthalin world graph

--- v3/src/data/world/index.ts
+++ v3/src/data/world/index.ts
@@ -1,10 +1,11 @@
 import type { WorldDataset } from '../../core/world/types';
 import { misthalinWorld } from './misthalin';
+import { misthalinTaskLocationWorld } from './misthalinTaskLocations';
 
 export const worldData: WorldDataset = {
-  towns: [...misthalinWorld.towns],
-  locations: [...misthalinWorld.locations],
-  edges: [...misthalinWorld.edges],
+  towns: [...misthalinWorld.towns, ...misthalinTaskLocationWorld.towns],
+  locations: [...misthalinWorld.locations, ...misthalinTaskLocationWorld.locations],
+  edges: [...misthalinWorld.edges, ...misthalinTaskLocationWorld.edges],
 };
 
 export const townById = new Map(worldData.towns.map((town) => [town.id, town]));
