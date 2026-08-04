96ef9475121af1c7a6174e862726a49606b67319
brandonbeem <130791945+brandonbeem@users.noreply.github.com>
2026-08-04 00:11:15 -0500

    Sprint 1: connect app to global player provider

--- v3/src/main.tsx
+++ v3/src/main.tsx
@@ -1,6 +1,7 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App';
+import { PlayerProvider } from './core/player/PlayerProvider';
 import './styles.css';
 
 const rootElement = document.getElementById('root');
@@ -11,6 +12,8 @@ if (!rootElement) {
 
 createRoot(rootElement).render(
   <StrictMode>
-    <App />
+    <PlayerProvider>
+      <App />
+    </PlayerProvider>
   </StrictMode>,
 );
