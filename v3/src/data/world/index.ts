c50c14d2e295c75891ade4c0655869e9c0d92170
brandonbeem <130791945+brandonbeem@users.noreply.github.com>
2026-08-04 12:21:48 -0500

    Include extended Misthalin route locations

--- v3/src/data/world/index.ts
+++ v3/src/data/world/index.ts
@@ -1,11 +1,24 @@
 import type { WorldDataset } from '../../core/world/types';
 import { misthalinWorld } from './misthalin';
 import { misthalinTaskLocationWorld } from './misthalinTaskLocations';
+import { misthalinExtendedLocationWorld } from './misthalinExtendedLocations';
 
 export const worldData: WorldDataset = {
-  towns: [...misthalinWorld.towns, ...misthalinTaskLocationWorld.towns],
-  locations: [...misthalinWorld.locations, ...misthalinTaskLocationWorld.locations],
-  edges: [...misthalinWorld.edges, ...misthalinTaskLocationWorld.edges],
+  towns: [
+    ...misthalinWorld.towns,
+    ...misthalinTaskLocationWorld.towns,
+    ...misthalinExtendedLocationWorld.towns,
+  ],
+  locations: [
+    ...misthalinWorld.locations,
+    ...misthalinTaskLocationWorld.locations,
+    ...misthalinExtendedLocationWorld.locations,
+  ],
+  edges: [
+    ...misthalinWorld.edges,
+    ...misthalinTaskLocationWorld.edges,
+    ...misthalinExtendedLocationWorld.edges,
+  ],
 };
 
 export const townById = new Map(worldData.towns.map((town) => [town.id, town]));
