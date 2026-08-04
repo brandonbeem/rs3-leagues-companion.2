2c6c2955cc733c5d57db2d2a6a0af7941b99c7ef
brandonbeem <130791945+brandonbeem@users.noreply.github.com>
2026-08-04 12:05:35 -0500

    Merge expanded tasks into route clusters

--- v3/src/data/clusters/index.ts
+++ v3/src/data/clusters/index.ts
@@ -1,6 +1,15 @@
 import { misthalinTaskClusters } from './misthalin';
+import { misthalinExpansionAssignments } from './misthalinExpansionAssignments';
 
-export const taskClusters = [...misthalinTaskClusters];
+export const taskClusters = misthalinTaskClusters.map((cluster) => {
+  const addedTaskIds = misthalinExpansionAssignments[cluster.id] ?? [];
+  return {
+    ...cluster,
+    taskIds: [...cluster.taskIds, ...addedTaskIds],
+    recommendedTaskOrder: [...cluster.recommendedTaskOrder, ...addedTaskIds],
+  };
+});
+
 export const taskClusterById = new Map(taskClusters.map((cluster) => [cluster.id, cluster]));
 export const taskClusterByTaskId = new Map(
   taskClusters.flatMap((cluster) => cluster.taskIds.map((taskId) => [taskId, cluster] as const)),
